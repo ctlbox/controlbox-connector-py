3e2fa25bffb023f28aeb164f080d3e3a90a380ee
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-03-27 16:38:33 +0100

    adds setup.py for python installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='controlbox-connector-py',
+      version='0.0.1',
+      description='Application neutral support for controlbox instances in Pytyon.',
+      url='',
+      author='',
+      author_email='',
+      license='LGPL',
+      packages=['controlbox', 'controlbox.conduit', 'controlbox.config', 'controlbox.connector',
+                'controlbox.protocol', 'controlbox.support'],
+      zip_safe=False)
